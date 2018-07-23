47a6a1bb523366fcef862ac2ed416e7434e72fd6
maria <m.ranci@arpalombardia.it>
2018-07-23 09:44:42 +0000

    da R a Rscript

--- anagrafica_IRIS.sh
+++ anagrafica_IRIS.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #=============================================================================
-# Lo script è all'interno del container e lancia uno script R ogni 24h. 
+# Lo script è all'interno del container e lancia uno script R ogni 4h. 
 # Lo script R estrae le info di anagrafica per IRIS dal DBmeteo e le importa 
 # nella tabella anagraficasensori del DB postgres di IRIS. 
 #
 # 2018/01/05 MR
 #=============================================================================
-numsec=21600 # 6 ore 
+numsec=14400 # 6 ore 
 
 SECONDS=$numsec
 LOCKFILE='usr/local/src/myscripts/.lock'
@@ -15,7 +15,7 @@ while [ 1 ]
 do
 if [ $SECONDS -ge $numsec ]
 then
- /usr/bin/R --vanilla < anagrafica_IRIS.R
+ /usr/bin/Rscript anagrafica_IRIS.R
  SECONDS=0
 fi
 done
