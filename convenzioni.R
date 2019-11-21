2595d81441e59105d4ba705639c7b71b27542a7c
mariaranci <31917865+mariaranci@users.noreply.github.com>
2019-11-21 11:02:04 +0100

    da 10.10.0.6 a 10.10.0.19

--- convenzioni.R
+++ convenzioni.R
@@ -22,7 +22,7 @@ fileoutpng <- arguments[4]
 
 # [] COLLEGAMENTO AL DB
 drv<-dbDriver("MySQL")
-conn<-try(dbConnect(drv, user="guardone", password=as.character(Sys.getenv("MYSQL_PWD")), dbname="METEO", host="10.10.0.6"))
+conn<-try(dbConnect(drv, user="guardone", password=as.character(Sys.getenv("MYSQL_PWD")), dbname="METEO", host="10.10.0.19"))
 
 if (inherits(conn,"try-error")) {
   print( "ERRORE nell'apertura della connessione al DBmeteo \n")
