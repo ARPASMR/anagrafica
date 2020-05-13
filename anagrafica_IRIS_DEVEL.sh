f93b968ef587b805fc8a13ce7b798a020a766973
S-Grasso <59735407+S-Grasso@users.noreply.github.com>
2020-05-13 11:22:36 +0200

    Update and rename anagrafica_IRIS.sh to anagrafica_IRIS_DEVEL.sh
    Aggiornamento dello script per la versione di sviluppo (DEVEL) in modo che giri una sola volta al giorno

---
+++ anagrafica_IRIS_DEVEL.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===================================================================================================================
+# Lo script è all'interno del container e lancia lo script script R "anagrafica_IRIS_DEVEL.R" una volta al giorno 
+# Lo script R estrae le info di anagrafica per IRIS "versione DEVEL" dal DBmeteo e 
+# le importa nella tabella anagraficasensori del DB postgres iris_devel
+#
+# 2020/05/12 SGR
+# 
+#==================================================================================================================
+numsec=24*3600 # 1volta al giorno
+/usr/bin/Rscript anagrafica_IRIS_DEVEL.R
+sleep $numsec
+while [ 1 ]
+do
+  if [ $SECONDS -ge $numsec ]
+  then
+    /usr/bin/Rscript anagrafica_IRIS_DEVEL.R
+    SECONDS=0
+    sleep $numsec
+  fi
+done
